1f6d54a1edacdcbc9d488adf1c810169269e2e39
Tom Stewart <github@tastewar.com>
2019-08-30 15:34:39 -0400

    Update Schedules201920.h
    Final corrected schedule for Ottoson

--- Code/classroomClock/Schedules201920.h
+++ Code/classroomClock/Schedules201920.h
@@ -7,9 +7,9 @@ const PROGMEM BellSched NormalDay=
     { 9,42, 9,52, ASPIRE,       ASPIRE,       ASPIRE,       ASPIRE},
     { 9,54,10,42, OUTOFCLUSTER, YELLOW,       YELLOW,       RED},
     {10,44,11,32, GREEN,        GREEN,        GREEN,        GREEN},
-    {11,34,11,56, BLUE,         PURPLE,       OUTOFCLUSTER, BLUE},
-    {11,58,12,20, BLUE,         PURPLE,       OUTOFCLUSTER, BLUE},
-    {12,22,12,44, BLUE,         PURPLE,       OUTOFCLUSTER, BLUE},
+    {11,34,11,57, LUNCH,        PURPLE,       OUTOFCLUSTER, BLUE},
+    {11,59,12,21, BLUE,         PURPLE,       OUTOFCLUSTER, LUNCH},
+    {12,23,12,46, BLUE,         LUNCH,        OUTOFCLUSTER, BLUE},
     {12,48,13,36, PURPLE,       OUTOFCLUSTER, BLUE,         OUTOFCLUSTER},
     {13,38,14,26, OUTOFCLUSTER, BLUE,         PURPLE,       PURPLE},
   }
@@ -52,8 +52,8 @@ const PROGMEM BellSched ER1300=
     { 9,18, 9,55, OUTOFCLUSTER, YELLOW,       YELLOW,       RED},
     { 9,57,10,34, GREEN,        GREEN,        GREEN,        GREEN},
     {10,36,11,12, BLUE,         PURPLE,       OUTOFCLUSTER, BLUE},
-    {11,14,12, 0, BLUE,         PURPLE,       OUTOFCLUSTER, BLUE},
-    {11,14,12, 0, BLUE,         PURPLE,       OUTOFCLUSTER, BLUE},
+    {11,14,11,36, PURPLE,       OUTOFCLUSTER, BLUE,         OUTOFCLUSTER},
+    {11,38,12, 0, PURPLE,       OUTOFCLUSTER, BLUE,         OUTOFCLUSTER},
     {12, 2,12,22, PURPLE,       OUTOFCLUSTER, BLUE,         OUTOFCLUSTER},
     {12,24,13, 0, OUTOFCLUSTER, BLUE,         PURPLE,       PURPLE},
   }
